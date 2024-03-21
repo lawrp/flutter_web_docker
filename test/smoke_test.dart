f119c88bcb99d32bb02c11a5356f0b45eedfe492
lawrp <rlaw.mercer@gmail.com>
2024-03-21 17:46:43 -0400

    update tests

--- test/smoke_test.dart
+++ test/smoke_test.dart
@@ -6,35 +6,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:basic/main.dart';
 
 void main() {
-  testWidgets('smoke test', (tester) async {
-    // Build our game and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that the 'Play' button is shown.
-    expect(find.text('Play'), findsOneWidget);
-
-    // Verify that the 'Settings' button is shown.
-    expect(find.text('Settings'), findsOneWidget);
-
-    // Go to 'Settings'.
-    await tester.tap(find.text('Settings'));
-    await tester.pumpAndSettle();
-    expect(find.text('Music'), findsOneWidget);
-
-    // Go back to main menu.
-    await tester.tap(find.text('Back'));
-    await tester.pumpAndSettle();
-
-    // Tap 'Play'.
-    await tester.tap(find.text('Play'));
-    await tester.pumpAndSettle();
-    expect(find.text('Select level'), findsOneWidget);
-
-    // Tap level 1.
-    await tester.tap(find.text('Level #1'));
-    await tester.pumpAndSettle();
-
-    // Find the first level's "tutorial" text.
-    expect(find.text('Drag the slider to 5% or above!'), findsOneWidget);
+  // Define a test. The TestWidgets function also provides a WidgetTester
+  // to work with. The WidgetTester allows you to build and interact
+  // with widgets in the test environment.
+  testWidgets('MyWidget has a title and message', (tester) async {
+    // Test code goes here.
   });
 }
